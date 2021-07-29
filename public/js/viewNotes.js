7e2bf7983ebbb9c2b599bbafd051fdb8ca3daaa3
Ara Oladipo <Oladipoeyiara@gmail.com>
2021-07-29 19:42:47 +0000

    Worked on password feature

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -1,41 +1,47 @@
 let googleUserId;
 
 window.onload = (event) => {
-  // Use this to retain user state between html pages.
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log('Logged in as: ' + user.displayName);
-     googleUserId  = user.uid;
-      getNotes(googleUserId);
-    } else {
-      // If not logged in, navigate back to login page.
-      window.location = 'index.html'; 
-    };
-  });
+    // Use this to retain user state between html pages.
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log('Logged in as: ' + user.displayName);
+            googleUserId = user.uid;
+            getNotes(googleUserId);
+        } else {
+            // If not logged in, navigate back to login page.
+            window.location = 'index.html';
+        };
+    });
 };
 
 const getNotes = (userId) => {
-  const notesRef = firebase.database().ref(`users/${userId}`);
-  notesRef.on('value', (snapshot) => {
-    const data = snapshot.val();
-    renderDataAsHtml(data);
-  });
+    const notesRef = firebase.database().ref(`users/${userId}`);
+    notesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        renderDataAsHtml(data);
+    });
 };
 
 const renderDataAsHtml = (data) => {
-  let cards = ``;
-  for(const noteItem in data) {
-    // console.log(data[noteItem].title[0])
-    const note = data[noteItem];
-    // For each note create an HTML card
-    cards += createCard(note, noteItem)
-  };
-  // Inject our string of HTML into our viewNotes.html page
-  document.querySelector('#app').innerHTML = cards;
+    let cards = ``;
+    for (const noteItem in data) {
+        // console.log(data[noteItem].title[0])
+        const note = data[noteItem];
+        // For each note create an HTML card
+        cards += createCard(note, noteItem)
+    };
+    // Inject our string of HTML into our viewNotes.html page
+    document.querySelector('#app').innerHTML = cards;
 };
 
 const createCard = (note, noteId) => {
-   return `
+    if(note.password){
+   return  `
+    <button class="button is-danger"  onclick="shownote('${noteId}', '${note.title}', '${note.text}', '${note.archive}')">Enter password</button>
+   
+   `
+    } else if(note.archive !== true ){
+    return `
      <div class="column is-one-quarter">
        <div class="card">
          <header class="card-header">
@@ -52,58 +58,83 @@ const createCard = (note, noteId) => {
          onclick="editNote('${noteId}')">Edit</a>
          
             <a href="#" class="card-footer-item"
-         onclick="archive('${noteId}, ${note.title}, ${note.text}')">Archive</a></footer>
+         onclick="archive('${noteId}', '${note.title}', '${note.text}')">Archive</a></footer>
        </div>
      </div>
-   `;
+   `
+    } else {
+        return ''
+    };
 };
 
-function deleteNote(noteId){
-  const val =  prompt("If you sure you want to delete this, type exactly 'Yes'" );
-  val === 'Yes' ? firebase.database().ref( `users/${googleUserId}/${noteId}`).remove() : alert("This not won't be deleted")
-  console.log(noteId)
+function deleteNote(noteId) {
+    const val = prompt("If you sure you want to delete this, type exactly 'Yes'");
+    val === 'Yes' ? firebase.database().ref(`users/${googleUserId}/${noteId}`).remove() : alert("This not won't be deleted")
+    console.log(noteId)
 }
 
-function editNote(noteId){
+function editNote(noteId) {
     const editNoteModal = document.querySelector("#editNoteModal");
-    const notesRef =  firebase.database().ref( `users/${googleUserId}/${noteId}`);
-    notesRef.on("value", (snapshot) =>{
+    const notesRef = firebase.database().ref(`users/${googleUserId}/${noteId}`);
+    notesRef.on("value", (snapshot) => {
         const note = snapshot.val();
         document.querySelector("#editTitleInput").value = note.title;
         document.querySelector("#editTextInput").value = note.text
         document.querySelector("#noteId").value = noteId
-    
+
     })
     editNoteModal.classList.toggle("is-active")
 }
 
-function saveEditedNote(){
-    const title = document.querySelector("#editTitleInput").value ;
+function saveEditedNote() {
+    const title = document.querySelector("#editTitleInput").value;
     const text = document.querySelector("#editTextInput").value;
     const noteId = document.querySelector("#noteId").value
-     const editedNote={
-        title, 
+    const editedNote = {
+        title,
         text
     }
 
-    firebase.database().ref( `users/${googleUserId}/${noteId}`).update(editedNote)
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update(editedNote)
     editNoteModal.classList.toggle("is-active")
 
 
 }
 
-function closeEditModal(){
-        const editNoteModal = document.querySelector("#editNoteModal");
-        editNoteModal.classList.toggle("is-active")
+function closeEditModal() {
+    const editNoteModal = document.querySelector("#editNoteModal");
+    editNoteModal.classList.toggle("is-active")
 
 }
 
-function archive(noteId, title, text){
-    console.log()
-  const thing =  firebase.database().ref( `users/${googleUserId}/archive`) 
-  console.log(thing)
-   
+function archive(noteId, title, text) {
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update({
+        title: title,
+        text: text,
+        archive: true,
+        password: ''
+    })
+};
 
-firebase.database().ref( `users/${googleUserId}/${noteId}`).remove()
+
+function shownote(noteId, title, text, archive){
+    const inp = String(prompt("What is the password"));
+   console.log()
+    const notesRef = firebase.database().ref(`users/${googleUserId}/${noteId}`);
+    notesRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data.password, String(inp))
+        if(data.password === String(inp)){
+  firebase.database().ref(`users/${googleUserId}/${noteId}`).update({
+        title: title,
+        text: text,
+        archive: false,
+        password: ''
+    })
+        }
+    });
 
 }
+
+// firebase.database().ref( `users/${googleUserId}/${noteId}`).remove()
+
--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -16,11 +16,15 @@ const handleNoteSubmit = () => {
   // 1. Capture the form data
   const noteTitle = document.querySelector('#noteTitle');
   const noteText = document.querySelector('#noteText');
+  const password = document.querySelector('#password');
   // 2. Format the data and write it to our database
-//   firebase.database().ref(`users/${googleUser.uid}`).push({
-//     title: noteTitle.value,
-//     text: noteText.value
-//   })
+  firebase.database().ref(`users/${googleUser.uid}`).push({
+    title: noteTitle.value,
+    text: noteText.value,
+    archive: false,
+    password: password.value
+  })
+  alert("Note created")
   // 3. Clear the form so that we can write a new note
 
 }
